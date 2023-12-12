03732a8a738c2dc2799cde4036ea1eaa81a833f4
Rogeriodona <rogerio.dona@outlook.com>
2023-12-12 15:49:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+
+let formulario = document.querySelector(".fale-conosco")
+let mascara =document.querySelector(".mascara-form ")
+
+
+
+function cliqueiNoBotao(){
+formulario.style.left = "700px"
+mascara.style.visibility = "visible"
+}
+
+function sumirFormulario(){
+    formulario.style.left = "-320"
+    mascara.style.visibility = "hidden"
+}
+
+
